8eb0d7b701fcd8a5319d36ba1c927df393a7e3b1
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-10-07 06:21:45 +0530

    Pattern2 Updated

--- Pattern1.py
+++ Pattern1.py
@@ -12,12 +12,26 @@ class patternPrinter:
             for j in range(n):
                 print("*",end = "")
             print()
-            
+      
+#Pattern 2     
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+
+''' 
+def pattern2(self,n):
+    for i in range(n):
+        for j in range(i+1):
+            print("*",end = " ") 
+        print()    
         
 def main():
     n = 5  
     printer = patternPrinter()
-    printer.pattern1(n)
+    printer.pattern2(n)
 
 if __name__ == "__main__":
     main()
