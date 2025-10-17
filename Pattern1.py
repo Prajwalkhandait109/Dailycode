e86899d67fbe8d31e3b71bf9469c7d6e22d70731
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-10-17 08:12:30 +0530

    pattern + element in range

--- Pattern1.py
+++ Pattern1.py
@@ -211,14 +211,28 @@ class patternPrinter:
                 
             print()
     
+    '''
+    1
+    2 3
+    4 5 6 
+    7 8 9 10
+    11 12 13 14 15
+    '''
     
+    def pattern13(self,n):
+        count = 1
+        for i in range(n+1):
+            for j in range(i):
+                print(count,end = " ")
+                count+=1
+            print()
     
       
     
 def main():
     n = 5  
     printer = patternPrinter()
-    printer.pattern12(n)
+    printer.pattern13(n)
 
 if __name__ == "__main__":
     main()
